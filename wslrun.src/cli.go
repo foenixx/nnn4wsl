bbb6098089de206f97209513d08b28e0d104fe13
foenixx <foenixx@gmail.com>
2022-01-11 15:31:25 +0300

    another rewrite of wslrun

--- wslrun.src/cli.go
+++ wslrun.src/cli.go
@@ -3,9 +3,10 @@ package main
 import "github.com/alecthomas/kong"
 
 type CLI struct {
-	Env		bool 	 `help:"Use WSLR_BUFFER variable as input string and ignore command-line exec arguments." short:"e"`
+	Env     bool     `help:"Use WSLR_BUFFER variable as input string and ignore command-line exec arguments." short:"e"`
 	Gui     bool     `help:"Run in GUI mode."`
 	NoGui   bool     `help:"Run in console mode."`
+	Hidden  bool     `help:"Run with hidden console window (hack for VS Code)."`
 	Verbose bool     `help:"Print verbose logs. Also you can set WSLR_VERBOSE=1 environment variable to switch verbose output on."`
 	Argv    []string `arg:"" optional:"" passthrough:""`
 }
--- wslrun.src/wslrun.go
+++ wslrun.src/wslrun.go
@@ -31,65 +31,80 @@ func initLog(verbose bool) {
 	}
 }
 
-func getCmdPathArgs(cmd string) (string, string) {
-	// "comm.exe " param1 param2
-	// " c:\program files\command\comm.exe  "
-	// comm.exe param1 param2
-	var reCmdName = regexp.MustCompile(`^\s*(".+?"|'.+?'|\S+)\s*(.*)$`)
-
-	m := reCmdName.FindStringSubmatch(cmd)
-	if m == nil {
-		return "", ""
+// splitArgs splits argument list string into separate arguments.
+// For example, this string:
+//   "text w spaces" 'text w spaces'  text"text" "text"text text'text' 'text'text text w spaces
+// splits into args:
+//	"text w spaces",'text w spaces',text"text","text"text,text'text','text'text,text,w,spaces
+func splitArgs(cmd string) []string {
+	var reArgs = regexp.MustCompile(`(?:"(?:\\"|.)+?"\S*)|(?:'.+?'\S*)|(?:\S+)`)
+	var reQuotes = regexp.MustCompile(`^(".*")|('.*')$`)
+	var args []string
+	matches := reArgs.FindAllString(cmd, -1)
+	log.Info().Strs("matches", matches).Msg("parsing input")
+	args = make([]string, len(matches))
+	for i, m := range matches {
+		if reQuotes.MatchString(m) {
+			//split quotes
+			args[i] = m[1 : len(m)-1]
+		} else {
+			args[i] = m
+		}
 	}
-	if m[1][0] == '"' || m[1][0] == '\'' {
-		//strip quotes
-		m[1] = m[1][1 : len(m[1])-1]
-	}
-
-	return m[1], m[2]
-
+	log.Info().Strs("args", args).Msg("parsed")
+	return args
 }
 
 func wslPath2Win(path string) string {
 	if strings.HasPrefix(path, "/mnt/") {
 		// /mnt/c/some/path --> c:/some/path
-		return fmt.Sprintf("%s:%s", string(path[5]),path[6:])
+		return fmt.Sprintf("%s:%s", string(path[5]), path[6:])
 	}
 	// path is absolute?
 	if strings.HasPrefix(path, `/`) {
 		// append unc prefix like "\\wsl$\Ubuntu-20.04"
-		return os.Getenv("WSLR_UNC_PREFIX") + strings.ReplaceAll(path, `/`,`\`)
+		return os.Getenv("WSLR_UNC_PREFIX") + strings.ReplaceAll(path, `/`, `\`)
 	}
 	return path
 }
 
-func pwshStartProcess(gui bool, process string, args string) error {
-	var a = []string{"-Command", "Start-Process", "\"" + process + "\""}
+func pwshStartProcess(gui bool, hidden bool, process string, args []string) error {
+	var a = []string{"-Command", "Start-Process", "'" + process + "'"}
 	if !gui {
 		a = append(a, "-NoNewWindow", "-Wait")
 	}
-	if args != "" {
-		// Input:  wslrun go build -ldflags="-s -w"
-		// Output: pwsh.exe -Command Start-Process go -ArgumentList "build -ldflags=`"-s -w`""
-		l := `"` + strings.ReplaceAll(args, "\"", "`\"") + `"`
-		a = append(a, "-ArgumentList", l)
+	if hidden {
+		// Visual Studio Code fix
+		// https://mypowershellnotes.wordpress.com/2020/04/26/i-cant-close-my-shell-until-i-close-visual-studio-code/
+		// https://stackoverflow.com/questions/57335812/how-to-open-visual-studio-code-through-powershell-and-close-powershell-right-aft
+		a = append(a, "-WindowStyle Hidden")
 	}
+
+	if len(args) > 0 {
+		// Input:  wslrun go build -ldflags="-s -w"
+		// Output: pwsh.exe -Command Start-Process 'go' -ArgumentList 'build','-ldflags="-s -w"'
+		a = append(a, "-ArgumentList '"+args[0]+"'")
+		for _, arg := range args[1:] {
+			a = append(a, ",'"+arg+"'")
+		}
+	}
+	//a = append(a, "| Out-Null")
 	log.Info().Strs("args", a).Msg("powershell process arguments")
 
 	return pwsh(a...)
 }
 
 func pwsh(args ...string) error {
-	pwsh, err := exec.LookPath("pwsh.exe")
+	exe, err := exec.LookPath("pwsh.exe")
 	if err != nil {
 		log.Info().Msg("cannot find pwsh.exe in the PATH, trying to use environment variable WSLR_PWSH")
-		pwsh = os.Getenv("WSLR_PWSH")
-		if pwsh == "" {
+		exe = os.Getenv("WSLR_PWSH")
+		if exe == "" {
 			log.Error().Msg("cannot find pwsh.exe, giving up...")
 			os.Exit(1)
 		}
 	}
-	cmd := exec.Command(pwsh, args...)
+	cmd := exec.Command(exe, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -103,9 +118,9 @@ func pwsh(args ...string) error {
 
 func main() {
 	var buf string
+	var args []string
 	cli := cliParse()
 	initLog(cli.Verbose)
-
 	if cli.Env {
 		buf = os.Getenv("WSLR_BUFFER")
 		if buf == "" {
@@ -114,19 +129,20 @@ func main() {
 			os.Exit(1)
 		}
 		log.Info().Str("buf", buf).Msg("WSLR_BUFFER value")
+		args = splitArgs(buf)
 	} else {
 		if len(cli.Argv) == 0 {
 			log.Error().Msg("no execution parameters passed")
 			os.Exit(1)
 		}
 		log.Info().Strs("argv", cli.Argv).Msg("exec arguments")
-		buf = strings.Join(cli.Argv, " ")
+		args = cli.Argv
 	}
-	path, args := getCmdPathArgs(buf)
+
 	// trying to translate wsl path into win path
-	path = wslPath2Win(path)
+	path := wslPath2Win(args[0])
 	ext := filepath.Ext(path)
-	log.Info().Str("path", path).Str("args", args).Str("file ext", ext).Msg("command parameters")
+	log.Info().Str("path", path).Strs("args", args[1:]).Str("file ext", ext).Msg("command parameters")
 
 	var err error
 	var gui bool
@@ -144,7 +160,7 @@ func main() {
 		//force console mode
 		gui = false
 	}
-	err = pwshStartProcess(gui, path, args)
+	err = pwshStartProcess(gui, cli.Hidden, path, args[1:])
 
 	if err != nil {
 		log.Error().Err(err).Msg("powershell launch error")
